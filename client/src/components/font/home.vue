f3e054eae06e3a4c05f740f4e4378b99552770b5
A-zliang <1365582147@qq.com>
2019-05-29 22:39:37 +0800

    增加文件表格的显示

--- client/src/components/font/home.vue
+++ client/src/components/font/home.vue
@@ -4,9 +4,14 @@
     <div class="home">
       <div class="show">
         <div class="tip">
-          <span class="left">微课视频</span> 
-          <router-link to="/moreVideo"><span class="right">更多<i class="iconfont el-icon-dzgengduo"></i></span></router-link> 
-         <hr>
+          <span class="left">微课视频</span>
+          <router-link to="/moreVideo">
+            <span class="right">
+              更多
+              <i class="iconfont el-icon-dzgengduo"></i>
+            </span>
+          </router-link>
+          <hr>
         </div>
         <div class="videos">
           <el-row>
@@ -30,9 +35,87 @@
             </el-col>
           </el-row>
         </div>
+
+        <div class="tip more-file">
+          <span class="left">课程文件</span>
+          <router-link to="/moreVideo">
+            <span class="right">
+              更多
+              <i class="iconfont el-icon-dzgengduo"></i>
+            </span>
+          </router-link>
+          <hr>
+        </div>
+
+        <div class="file">
+          <el-table :data="fileList" style="width: 100%">
+            <el-table-column label="文件名" width="200">
+              <template slot-scope="scope">
+                <i v-if="'pptx'==scope.row.ext" class="iconfont el-icon-dzppt"></i>
+                <i
+                  v-else-if="'xlsx'==scope.row.ext||'xls'==scope.row.ext"
+                  class="iconfont el-icon-dzExcel"
+                ></i>
+                <i
+                  v-else-if="'doc'==scope.row.ext||'docx'==scope.row.ext"
+                  class="iconfont el-icon-dzword"
+                ></i>
+                <i v-else-if="'zip'==scope.row.ext" class="iconfont el-icon-dzzip"></i>
+                <i v-else-if="'pdf'==scope.row.ext" class="iconfont el-icon-dzpdf"></i>
+                <i v-else-if="'txt'==scope.row.ext" class="iconfont el-icon-dztxt"></i>
+                <i
+                  v-else-if="'jpg'==scope.row.ext||'png'==scope.row.ext"
+                  class="iconfont el-icon-dztupian"
+                ></i>
+                <i v-else class="iconfont el-icon-dzweizhiwenjian"></i>
+                <span
+                  style="margin-left: 10px"
+                  @click="downloadFiles(scope.row)"
+                >{{ scope.row.topic }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="create_time" label="上传时间" :formatter="dateFormat" width="180"></el-table-column>
+            <el-table-column prop="username" label="上传者"></el-table-column>
+            <el-table-column prop="username" label="下载">
+              <template slot-scope="scope">
+                <i class="iconfont el-icon-dzxiazai" @click="downloadFiles(scope.row)"></i>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="btn">
+            <el-button @click="filePanel=true">分享我的文件</el-button>
+          </div>
+        </div>
       </div>
     </div>
 
+    <el-dialog title="分享你的文件" :visible.sync="filePanel">
+       <el-form label-width="80px">
+          <el-form-item label="内容介绍">
+              <el-input v-model="file.topic"></el-input>
+          </el-form-item>
+      </el-form>
+      <el-upload
+        class="upload-demo"
+        style="margin-left:80px"
+        drag
+        name="file"
+        action="#"
+        :auto-upload="false"
+        :http-request="uploadFile"
+        ref="uploadFile"
+        multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">
+          将文件拖到此处，或
+          <em>点击上传</em>
+        </div>
+      </el-upload>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="filePanel = false">取 消</el-button>
+        <el-button type="primary" @click="uploadFile">确 定</el-button>
+      </div>
+    </el-dialog>
     <foot></foot>
   </div>
 </template>
@@ -41,27 +124,137 @@
 import headtop from "./head";
 import foot from "./footer";
 import videos from "./video";
+import paginator from "./paginator";
+
 export default {
   data() {
     return {
+      filePanel: false,
       videoList: [],
+      fileList: [],
       total: 0,
       pagination: {
         page: 1,
         size: 6,
         total: 0
-      }
+      },
+      file:{
+        topic:'',
+        username:""
+    },
     };
   },
   created() {
     this.getVideoMsg();
+    this.getFiles();
   },
-  components: { headtop, foot, videos },
+  components: { headtop, foot, videos, paginator },
   methods: {
+          async uploadFile(){
+                if(this.file.topic==''){
+                    alert('请填写主题');
+                    return;
+                }
+                this.file.username =  this.$store.state.user.user_name;
+                console.log(this.file);
+                const  formData = new FormData();
+                const file = this.$refs.uploadFile.uploadFiles;
+                const headerConfig = { headers: { 'Content-Type': 'multipart/form-data' }};
+                 if (!file) {
+                    alert('请选择文件');
+                    return;
+                }
+                for(let i=0; i<file.length; i++){
+                     formData.append('file', file[i].raw);
+                }
+                console.log(file);
+                formData.append('length', file.length);
+                formData.append('topic', this.file.topic);
+                formData.append('username', this.file.username);
+                let res = await this.$http.api_upload_file(formData,headerConfig);
+                console.log(res);
+                if(res.data.code == 200){
+                    this.getFiles();
+                    alert(res.data.msg);
+                    this.filePanel =false;
+                    this.$refs.uploadFile.clearFiles();
+                    this.file.topic = '';
+                }
+            },
+           async sendFile(){
+
+            },
     async getVideoMsg() {
       let res = await this.$http.api_get_VideoMsg(this.pagination);
       if (res.data.code == 200) {
         this.videoList = res.data.res;
+      }
+    },
+    async getFiles() {
+      this.fileList = [];
+      let res = await this.$http.api_get_files();
+      let temp = {
+        create_time: "",
+        ext: "",
+        filePath: "",
+        username: "",
+        _id: ""
+      };
+      if (res.data.code == 200) {
+        for (let i = 0; i < 10; i++) {
+          temp.create_time = res.data.res[res.data.res.length-1-i].create_time;
+          temp.filePath = res.data.res[res.data.res.length-1-i].filePath;
+          temp.topic = res.data.res[res.data.res.length-1-i].topic;
+          temp.username = res.data.res[res.data.res.length-1-i].username;
+          temp._id = res.data.res[res.data.res.length-1-i]._id;
+          let index = res.data.res[res.data.res.length-1-i].filePath.lastIndexOf(".");
+          temp.ext = res.data.res[res.data.res.length-1-i].filePath.substring(index + 1);
+          this.fileList.push(temp);
+          temp = {
+            create_time: "",
+            filePath: "",
+            username: "",
+            _id: "",
+            ext: ""
+          };
+        }
+      }
+    },
+    async downloadFiles(data) {
+      let _id = data._id;
+      window.open(`/api/getfile?id=${_id}`, "_self");
+    },
+    dateFormat(row, column, cellValue, index) {
+      //此方法对表格中的时间进行格式化，容易拿到表格列出来循环的数据
+      const daterc = row[column.property];
+      if (daterc != null) {
+        const dateMat = new Date(
+          parseInt(daterc.replace("/Date(", "").replace(")/", ""), 10)
+        );
+        let year = dateMat.getFullYear();
+        let month = dateMat.getMonth() + 1;
+        if (month < 10) {
+          month = "0" + month;
+        }
+        let day = dateMat.getDate();
+        if (day < 10) {
+          day = "0" + day;
+        }
+        let hh = dateMat.getHours();
+        if (hh < 10) {
+          hh = "0" + hh;
+        }
+        let mm = dateMat.getMinutes();
+        if (mm < 10) {
+          mm = "0" + mm;
+        }
+        let ss = dateMat.getSeconds();
+        if (ss < 10) {
+          ss = "0" + ss;
+        }
+        const timeFormat =
+          year + "-" + month + "-" + day + " " + hh + ":" + mm + ":" + ss;
+        return timeFormat;
       }
     }
   }
@@ -87,20 +280,20 @@ h1 {
   width: 1263px;
   position: relative;
 }
-.show .tip hr{
+.show .tip hr {
   box-shadow: 2px 2px 2px #333;
 }
 .show .tip .left {
   font-size: 25px;
-  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 }
-.show .tip .right{
+.show .tip .right {
   position: absolute;
   color: #333;
   bottom: 5px;
   right: 0;
 }
-.show .tip .right:hover{
+.show .tip .right:hover {
   color: red;
 }
 .image {
@@ -122,6 +315,9 @@ h1 {
 .el-col {
   margin: 25px 25px;
   cursor: pointer;
+}
+.videos {
+  background-color: #fff;
 }
 .videos .desc {
   padding: 14px;
@@ -148,6 +344,20 @@ h1 {
   opacity: 0.75;
   line-height: 300px;
   margin: 0;
+}
+.more-file {
+  margin: 30px 0 0 0;
+}
+.file {
+  padding: 25px;
+  background-color: #fff;
+}
+.el-icon-dzxiazai:hover {
+  color: red;
+}
+.file .btn {
+  margin: 10px 0;
+  right: 0;
 }
 </style>
 
